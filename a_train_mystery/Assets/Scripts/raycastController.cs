346e3e8b5b8ba63fdc43ff21dc2184e9aa40f065
Enes <enescubuk10@gmail.com>
2022-12-24 02:08:00 +0300

    raycast

---
+++ a_train_mystery/Assets/Scripts/raycastController.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using UnityEngine;
+
+public class raycastController : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
