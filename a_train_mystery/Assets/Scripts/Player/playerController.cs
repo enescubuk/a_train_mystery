d46a70716ca9df5d475ccee9a09cacdf4263089c
Enes <enescubuk10@gmail.com>
2022-12-24 01:55:32 +0300

    playerControl fix

--- a_train_mystery/Assets/Scripts/Player/playerController.cs
+++ a_train_mystery/Assets/Scripts/Player/playerController.cs
@@ -24,13 +24,19 @@ public class playerController : MonoBehaviour
     void gatherInput()
     {
         controlAxis = new Vector3(Input.GetAxisRaw("Horizontal"),0,Input.GetAxisRaw("Vertical"));
+        controlAxis.Normalize();
     }
 
     void Look()
     {
         if (controlAxis != Vector3.zero)
         {
-            var releative = (transform.position + controlAxis) - transform.position;
+            var matrix = Matrix4x4.Rotate(Quaternion.Euler(0,45,0));
+
+            var skewedInput = matrix.MultiplyPoint3x4(controlAxis);
+
+
+            var releative = (transform.position + skewedInput) - transform.position;
             var rot = Quaternion.LookRotation(releative,Vector3.up);
 
             transform.rotation = Quaternion.RotateTowards(transform.rotation,rot,turnSpeed * Time.deltaTime);
